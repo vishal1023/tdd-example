4d3a7204298336bbd2d4f6d2818a73bc0d019b75
vishal <vishal.akkalkote@spark.net>
2022-01-06 18:11:04 +0100

    fix the test by adding the exception handler

--- src/main/java/com/example/tdd/api/CarController.java
+++ src/main/java/com/example/tdd/api/CarController.java
@@ -1,11 +1,10 @@
 package com.example.tdd.api;
 
+import com.example.tdd.exception.CarNotFoundException;
 import com.example.tdd.model.Car;
 import com.example.tdd.service.CarService;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CarController {
@@ -20,4 +19,8 @@ public class CarController {
     private Car getCar(@PathVariable String name) {
         return carService.getCarDetails(name);
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private void carNotFoundHandler(CarNotFoundException e) {}
 }
