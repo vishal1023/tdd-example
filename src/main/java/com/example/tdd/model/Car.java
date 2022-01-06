2089bfd1d778319d099c07eecf13d9f50bd79e04
vishal <vishal.akkalkote@spark.net>
2022-01-06 13:33:36 +0100

    Add integration test for the new endpoint

---
+++ src/main/java/com/example/tdd/model/Car.java
@@ -0,0 +1,11 @@
+package com.example.tdd.model;
+
+public class Car {
+    public String getName() {
+        return null;
+    }
+
+    public String getType() {
+        return null;
+    }
+}
---
+++ src/test/java/com/example/tdd/IntegrationTest.java
@@ -0,0 +1,36 @@
+package com.example.tdd;
+
+import com.example.tdd.model.Car;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+
+public class IntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void getCars_shouldReturnCarDetails() {
+        //arrange
+
+        //act
+        ResponseEntity<Car> response = restTemplate.getForEntity("/cars/bmw", Car.class);
+
+        //assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getName()).isEqualTo("bmw");
+        assertThat(response.getBody().getType()).isEqualTo("hybrid");
+    }
+}
