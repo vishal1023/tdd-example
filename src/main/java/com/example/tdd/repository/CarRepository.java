7b4f4d605ae0995388cd604fcb907d83a6caa209
vishal <vishal.akkalkote@spark.net>
2022-01-06 18:29:06 +0100

    Add dummy values to the repository

--- src/main/java/com/example/tdd/repository/CarRepository.java
+++ src/main/java/com/example/tdd/repository/CarRepository.java
@@ -3,10 +3,26 @@ package com.example.tdd.repository;
 import com.example.tdd.model.Car;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Repository
 public class CarRepository {
 
+    private final Map<String, Car> cars;
+
+    public CarRepository() {
+        this.cars = new HashMap<>();
+        initCarMapping(cars);
+    }
+
+    private void initCarMapping(Map<String, Car> cars) {
+        cars.put("bmw", new Car("bmw", "hybrid"));
+        cars.put("volkswagen", new Car("volkswagen", "petrol"));
+        cars.put("audi", new Car("audi", "diesel"));
+    }
+
     public Car findByName(String name) {
-        return null;
+        return cars.get(name);
     }
 }
