827bc62eb3bbd7d598b2a3a83dff093d7e59d832
vishal <vishal.akkalkote@spark.net>
2022-01-06 17:11:32 +0100

    Create CarController with /cars/{name} endpoint

--- src/main/java/com/example/tdd/api/CarController.java
+++ src/main/java/com/example/tdd/api/CarController.java
@@ -1,4 +1,15 @@
 package com.example.tdd.api;
 
+import com.example.tdd.model.Car;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CarController {
+
+    @GetMapping("/cars/{name}")
+    public Car getCar(@PathVariable String name) {
+        return null;
+    }
 }
